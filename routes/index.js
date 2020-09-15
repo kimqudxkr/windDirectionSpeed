7473a7942c3d4904812839eaeb8c1410124e6c1b
tak <kimqudxkr@naver.com>
2020-09-15 16:53:39 +0900

    maria DB 연동

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const connection = require('./connection');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const query = 'SELECT no, title, writer, date FROM btboard';
+
+  connection.query(query, function(err, rows) {
+    if(!err) {
+      res.render('index',{result:rows});
+    } else {
+      console.log('query error : '+err);
+      res.send(err);
+    }
+  })
 });
 
 module.exports = router;

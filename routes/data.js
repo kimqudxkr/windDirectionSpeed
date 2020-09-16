4be804fd3089cfb63210c309547272bd7ba07bb7
tak <kimqudxkr@naver.com>
2020-09-16 13:16:58 +0900

    측정 자료 조회 페이지 완료

--- routes/data.js
+++ routes/data.js
@@ -3,41 +3,6 @@ var router = express.Router();
 const moment = require('moment');   
 const connection = require('./connection');
 
-const getDustStatus = (value) => {
-    if (value <= 30) {
-      return 'blue'
-    } 
-    else if (value > 30 && value <= 80) {
-      return 'green'
-    }
-    else if (value > 80 && value <= 150) {
-      return 'yellow'
-    }
-    else if (value > 150 && value <= 600) {
-      return 'red'
-    }
-    else {
-      return 'blue'
-    }
-  }
-  
-  const getUltraFineDustStatus = (value) => {
-    if (value <= 15) {
-      return 'blue'
-    } 
-    else if (value > 15 && value <= 35) {
-      return 'green'
-    }
-    else if (value > 35 && value <= 75) {
-      return 'yellow'
-    }
-    else if (value > 75 && value <= 500) {
-      return 'red'
-    }
-    else {
-      return 'blue'
-    }
-  }
 
   const selectOption = (params) => {
     switch (params.setting) {
@@ -65,30 +30,20 @@ const getDustStatus = (value) => {
   /* GET databyday page. */
   router.get('/', (req, res, next) => {  
     const query = `
-      SELECT MAX(pm10_0) AS dust, 
-             MAX(pm2_5) AS ultrafine, 
-             AVG(windDirection) AS windDirection, 
-             ROUND(AVG(substr(windSpeed, 1, 3)), 2) AS windSpeed, 
-             ROUND(AVG(temperature), 1) As temperature,
-             ROUND(AVG(humidity), 1) AS humidity,
+      SELECT AVG(windDirection) AS windDirection, 
+             ROUND(AVG(substr(windSpeed, 1, 3)), 2) AS windSpeed,
              rgst_dt 
       FROM finedust_tb 
       GROUP BY SUBSTR(rgst_dt, 1, 10) 
-      ORDER BY rgst_dt DESC LIMIT 30
+      ORDER BY rgst_dt DESC
     `;
   
     connection.query(query, (err, rows, fields) => {
       if (!err) {
         res.render('data', {'datas': rows.map(data => {
                                               return {
-                                                dust: data.dust,
-                                                dustStatus: getDustStatus(data.dust),
-                                                ultrafine: data.ultrafine,
-                                                ultrafineStatus: getUltraFineDustStatus(data.ultrafine),
                                                 windDirection: data.windDirection,
                                                 windSpeed: data.windSpeed,
-                                                temperature: data.temperature,
-                                                humidity: data.humidity,
                                                 rgst_dt: moment(data.rgst_dt).format('YYYY-MM-DD')
                                               }
                                             })
@@ -106,13 +61,9 @@ const getDustStatus = (value) => {
     let time = selectOption(params);
   
     let query = `
-          SELECT MAX(pm10_0) AS dust, 
-                MAX(pm2_5) AS ultrafine, 
-                AVG(windDirection) AS windDirection, 
-                ROUND(AVG(substr(windSpeed, 1, 3)), 2) AS windSpeed, 
-                ROUND(AVG(temperature), 1) As temperature,
-                ROUND(AVG(humidity), 1) AS humidity,
-                rgst_dt 
+          SELECT AVG(windDirection) AS windDirection, 
+                 ROUND(AVG(substr(windSpeed, 1, 3)), 2) AS windSpeed, 
+                 rgst_dt 
           FROM finedust_tb WHERE ${time} 
           GROUP BY SUBSTR(rgst_dt, 1, 10) 
           ORDER BY rgst_dt DESC 
@@ -127,16 +78,10 @@ const getDustStatus = (value) => {
           const html = `
             <tr>
               <td>${moment(data.rgst_dt).format(dateFormat['10'])}</td>
-              <td class="${getDustStatus(data.dust)}">●</td>
-              <td>${data.dust}</td>
-              <td class="${getUltraFineDustStatus(data.ultrafine)}">●</td>
-              <td>${data.ultrafine}</td>
               <td>
                 <img class="wind-direction-icon" src="images/Black_Arrow.png" alt="wind-direction" style="width: 40px; transform: rotate(${data.windDirection}deg);" />
               </td>
               <td>${data.windSpeed} (m/s)</td>
-              <td>${data.temperature} °C</td>
-              <td>${data.humidity} %</td>
             </tr>
           `
           result += html;

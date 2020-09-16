bd1b9f78bcd933ca4fef8f2efdcae26d519f04c4
tak <kimqudxkr@naver.com>
2020-09-17 08:43:26 +0900

    신시간 자료 조회 페이지 완성

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const moment = require('moment');   
 const connection = require('./connection');
 
 /* GET home page. */
@@ -7,13 +8,32 @@ router.get('/', function(req, res, next) {
   const query = 'SELECT no, title, writer, date FROM btboard';
 
   connection.query(query, function(err, rows) {
-    if(!err) {
-      res.render('index',{result:rows});
-    } else {
-      console.log('query error : '+err);
-      res.send(err);
-    }
-  })
+    const query = `
+      SELECT AVG(windDirection) AS windDirection, 
+             ROUND(AVG(substr(windSpeed, 1, 3)), 2) AS windSpeed,   
+             rgst_dt 
+      FROM finedust_tb 
+      GROUP BY SUBSTR(rgst_dt, 1, 13) 
+      ORDER BY rgst_dt DESC LIMIT 10
+    `;
+  
+    connection.query(query, (err, rows, fields) => {
+      if (!err) {
+        res.render('index', {'datas': rows.map(data => {
+                                              return {
+                                                windDirection: data.windDirection,
+                                                windSpeed: data.windSpeed,
+                                                rgst_dt: moment(data.rgst_dt).format('YYYY-MM-DD HH:MM:SS')
+                                              }
+                                            })
+                                    });
+      }
+      else {
+        console.log(err);
+        res.render('index', err);
+      }
+    })
+  });
 });
 
 module.exports = router;

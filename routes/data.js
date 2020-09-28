f8dfe011072343394f4d9d740f1bc2edffdc354e
tak <kimqudxkr@naver.com>
2020-09-28 16:30:54 +0900

    코드 수정 + 실시간 데이터 새로고침

--- routes/data.js
+++ routes/data.js
@@ -3,103 +3,94 @@ var router = express.Router();
 const moment = require('moment');   
 const connection = require('./connection');
 
-
-  const selectOption = (params) => {
-    switch (params.setting) {
-      case "user":
-        start = `${params.startDay} ${params.startHour}:00:00`;
-        end = `${params.endDay} ${params.endHour}:00:00`;
-        time = `rgst_dt >= '${start}' AND rgst_dt <= '${end}'`
-        return time;
-      case "quater":
-        time = `quarter(rgst_dt) = ${params.quater}`;
-        return time;
-      case "year":
-        time = `year(rgst_dt) = ${params.year}`;
-        return time;
-      case "month":
-        time = `month(rgst_dt) = ${params.month}`;
-        return time;
-      case "week":
-        time = `year(rgst_dt) = ${params.year} and month(rgst_dt) = ${params.month} and 
-                (week(rgst_dt) - week('${params.year}-${params.month}-01') + 1 = ${params.week})`
-        return time;
-    }
+const selectOption = (params) => {
+  switch (params.termSetting) {
+    case "user":
+      const start = `${params.startDay} ${params.startHour}:00:00`;
+      const end = `${params.endDay} ${params.endHour}:00:00`;
+      return `rgst_dt >= '${start}' AND rgst_dt <= '${end}'`;
+    case "quater":
+      return `quarter(rgst_dt) = ${params.quater}`;
+    case "year":
+      return `year(rgst_dt) = ${params.year}`;
+    case "month":
+      return `month(rgst_dt) = ${params.month}`;
+    case "week":
+      return `year(rgst_dt) = ${params.year} and month(rgst_dt) = ${params.month} and 
+              (week(rgst_dt) - week('${params.year}-${params.month}-01') + 1 = ${params.week})`;
   }
-  
-  /* GET databyday page. */
-  router.get('/', (req, res, next) => {  
-    const query = `
-      SELECT AVG(windDirection) AS windDirection, 
-             ROUND(AVG(substr(windSpeed, 1, 3)), 2) AS windSpeed,
-             rgst_dt 
-      FROM finedust_tb 
-      GROUP BY SUBSTR(rgst_dt, 1, 10) 
-      ORDER BY rgst_dt DESC
-    `;
-  
-    connection.query(query, (err, rows, fields) => {
-      if (!err) {
-        res.render('data', {'datas': rows.map(data => {
-                                              return {
-                                                windDirection: data.windDirection,
-                                                windSpeed: data.windSpeed,
-                                                rgst_dt: moment(data.rgst_dt).format('YYYY-MM-DD')
-                                              }
-                                            })
-                                    });
-      }
-      else {
-        console.log(err);
-        res.render('data', err);
-      }
-    })
-  })
-  
-  router.get('/api/search', (req, res, next) => {
-    const params = req.query;
-    let time = selectOption(params);
-  
-    let query = `
-          SELECT AVG(windDirection) AS windDirection, 
-                 ROUND(AVG(substr(windSpeed, 1, 3)), 2) AS windSpeed, 
-                 rgst_dt 
-          FROM finedust_tb WHERE ${time} 
-          GROUP BY SUBSTR(rgst_dt, 1, 10) 
-          ORDER BY rgst_dt DESC 
-        `;
-  
-    connection.query(query, (err, rows, fields) => {
-      if (!err) {
-        let result;
-        const dateFormat = {'13': 'MM-DD:HH', '10': 'YYYY-MM-DD', '18': 'MM-DD HH:mm:ss'};
+}
 
-        if(JSON.stringify(rows)=='[]') {
-          result = `
-          <tr>
-            <td colspan="3">No Data</td>
-          </tr>`
-        } else {      
-          rows.forEach(data => {
-            const html = `
-              <tr>
-                <td>${moment(data.rgst_dt).format(dateFormat['10'])}</td>
-                <td>
-                  <img class="wind-direction-icon" src="images/Black_Arrow.png" alt="wind-direction" style="width: 40px; transform: rotate(${data.windDirection}deg);" />
-                </td>
-                <td>${data.windSpeed} (m/s)</td>
-              </tr>
-            `
-            result += html;
-          });
-        } 
-        res.send(result);
-      }
-      else {
-        console.log(err);
-        res.send(err);
-      }
-    })
+/* GET databyday page. */
+router.get('/', (req, res, next) => {  
+  const query = `
+    SELECT AVG(windDirection) AS windDirection, 
+            ROUND(AVG(substr(windSpeed, 1, 3)), 2) AS windSpeed,
+            rgst_dt AS rgstDt
+    FROM finedust_tb 
+    GROUP BY SUBSTR(rgst_dt, 1, 10) 
+    ORDER BY rgst_dt DESC`;
+
+  connection.query(query, (err, rows, fields) => {
+    if (!err) {
+      res.render('data', {'datas': rows.map(data => {
+                                            return {
+                                              windDirection: data.windDirection,
+                                              windSpeed: data.windSpeed,
+                                              rgstDt: moment(data.rgstDt).format('YYYY-MM-DD')
+                                            }
+                                          })
+                          });
+    } 
+    else {
+      res.render('data', err);
+    }
   })
-  
-  module.exports = router;
+})
+
+router.get('/api/search', (req, res, next) => {
+  const params = req.query;
+  const time = selectOption(params);
+
+  const query = `
+    SELECT AVG(windDirection) AS windDirection, 
+            ROUND(AVG(substr(windSpeed, 1, 3)), 2) AS windSpeed, 
+            rgst_dt AS rgstDt
+    FROM finedust_tb WHERE ${time} 
+    GROUP BY SUBSTR(rgst_dt, 1, 10) 
+    ORDER BY rgst_dt DESC`;
+
+  connection.query(query, (err, rows, fields) => {
+    if (!err) {
+      let result;
+      const dateFormat = {'13': 'MM-DD:HH', '10': 'YYYY-MM-DD', '18': 'MM-DD HH:mm:ss'};
+
+      if(JSON.stringify(rows)=='[]') {
+        result = `
+        <tr>
+          <td colspan="3">No Data</td>
+        </tr>`
+      } 
+      else {      
+        rows.forEach(data => {
+          const html = `
+            <tr>
+              <td>${moment(data.rgstDt).format(dateFormat['10'])}</td>
+              <td>
+                <img class="wind-direction-icon" src="images/Black_Arrow.png" alt="wind-direction" style="width: 40px; transform: rotate(${data.windDirection}deg);" />
+              </td>
+              <td>${data.windSpeed} (m/s)</td>
+            </tr>
+          `
+          result += html;
+        });
+      } 
+      res.send(result);
+    } 
+    else {
+      res.send(err);
+    }
+  })
+})
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -3,53 +3,76 @@ var router = express.Router();
 const moment = require('moment');   
 const connection = require('./connection');
 
+const realTimeQuery = `SELECT windDirection, windSpeed, rgst_dt AS rgstDt
+                       FROM finedust_tb ORDER BY logIdx DESC LIMIT 1`;
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  const query = 'SELECT no, title, writer, date FROM btboard';
+  let latest;   //쿼리 결과를 저장하고 다음번 쿼리에서 함께 데이터를 보내기 위해 전역 선언
 
-  connection.query(query, function(err, rows) {
-    const query = `
-      SELECT AVG(windDirection) AS windDirection, 
-             ROUND(AVG(substr(windSpeed, 1, 3)), 2) AS windSpeed,   
-             rgst_dt 
-      FROM finedust_tb 
-      GROUP BY SUBSTR(rgst_dt, 1, 13) 
-      ORDER BY rgst_dt DESC LIMIT 10
-    `;
-    
-    let latest;
-    connection.query(`select windDirection, windSpeed, rgst_dt
-                      FROM finedust_tb ORDER BY logIdx DESC LIMIT 1`, (err, res) => {
-                        if(!err) {
-                          latest=res;
-                        } else {
-                          res.render('index',err);
-                        }
-                      })
-
-    connection.query(query, (err, rows, fields) => {
-      if (!err) {
-        res.render('index', {'datas': rows.map(data => {
-                                              return {
-                                                windDirection: data.windDirection,
-                                                windSpeed: data.windSpeed,
-                                                rgst_dt: moment(data.rgst_dt).format('MM-DD HH')
-                                              }
-                                            })
-                                            ,'latest': latest.map(data => {
-                                              return {
-                                                windDirection: data.windDirection,
-                                                windSpeed: data.windSpeed,
-                                                rgst_dt: moment(data.rgst_dt).format('YYYY-MM-DD HH:mm:SS')
-                                              }
-                                            })});
-      }
-      else {
-        console.log(err);
-        res.render('index', err);
-      }
-    })
+  connection.query(realTimeQuery, (err, rows) => {
+    if(!err) {
+      latest=rows;
+    } 
+    else {
+      res.render('index',err);
+    }
   });
+
+  const query = `
+    SELECT AVG(windDirection) AS windDirection, 
+          ROUND(AVG(substr(windSpeed, 1, 3)), 2) AS windSpeed,
+          rgst_dt AS rgstDt
+    FROM finedust_tb 
+    GROUP BY SUBSTR(rgst_dt, 1, 13) 
+    ORDER BY rgst_dt DESC LIMIT 10`;
+    
+  connection.query(query, (err, rows, fields) => {
+    if (!err) {
+      res.render('index', {'datas': rows.map(data => {
+                                            return {
+                                              windDirection: data.windDirection,
+                                              windSpeed: data.windSpeed,
+                                              rgstDt: moment(data.rgstDt).format('MM-DD HH')
+                                            }
+                                          })
+                                          ,'latest': latest.map(data => {
+                                            return {
+                                              windDirection: data.windDirection,
+                                              windSpeed: data.windSpeed,
+                                              rgstDt: moment(data.rgstDt).format('YYYY-MM-DD HH:mm:SS')
+                                            }
+                                          })
+                          });
+    } 
+    else {
+      res.render('index', err);
+    }
+  })
 });
+
+router.get('/api/updateData', (req, res, next) => {
+  connection.query(realTimeQuery, (err, rows) => {
+    if(!err) {
+      const html = `
+        <tr>
+          <th>시간</th>
+          <td>${rows[0].rgstDt}</td>
+        </tr>
+        <tr>
+          <th>풍향</th>
+          <td><img class="wind-direction-icon" src="images/Black_Arrow.png" alt="wind-direction" style="width: 40px; transform: rotate(${rows[0].windDirection}deg);" /></td>
+        </tr>
+        <tr>
+          <th>풍속</th>
+          <td>${rows[0].windSpeed}</td>
+        </tr>
+      `;
+      res.send(html);
+    } 
+    else {
+      res.render('index',err);
+    }
+  });
+})
 
 module.exports = router;

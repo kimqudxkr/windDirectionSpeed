bd63b4a1d0c66fbc42b653c7d4ea43ff817f117e
tak <kimqudxkr@naver.com>
2020-10-12 13:59:34 +0900

    moduleTable 생성 및 연결

--- routes/module.js
+++ routes/module.js
@@ -1,9 +1,26 @@
 var express = require('express');
+const connection = require('./connection');
+const moment = require('moment');   
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('module');
+  const query = 'SELECT * FROM moduleList';
+  
+  connection.query(query,(err, rows, fields) => {
+    if(!err) {
+      res.render('module',{'moduleList':rows.map(result => {
+                                                  return {
+                                                    deviceId: result.deviceId,
+                                                    location: result.location,
+                                                    rgstDt: moment(result.rgstDt).format('YYYY-MM-DD')
+                                                  }
+                                                })
+                            });
+    } else {
+      res.render('module',err);
+    }
+  })
 });
 
 module.exports = router;

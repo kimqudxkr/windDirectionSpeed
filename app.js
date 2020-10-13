94c31f6ff5149cca5b854c54adf40dc132b433ea
tak <kimqudxkr@naver.com>
2020-10-13 10:58:59 +0900

    등록 장치 수정 기능 추가

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dataRouter = require('./routes/data');
-var moduleRouter = require('./routes/module');
-var registModuleRouter = require('./routes/registModule');
+var moduleRouter = require('./routes/module');              
+var registModuleRouter = require('./routes/registModule');  
+var modifyModuleRouter = require('./routes/modifyModule');  //수정 페이지
 
 var app = express();
 
@@ -27,6 +28,7 @@ app.use('/users', usersRouter);
 app.use('/data', dataRouter);
 app.use('/module', moduleRouter);
 app.use('/registModule', registModuleRouter);
+app.use('/modifyModule', modifyModuleRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/modifyModule.js
@@ -0,0 +1,58 @@
+var express = require('express');
+const connection = require('./connection');
+const moment = require('moment');   
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  if(req.query.deviceId) {
+    const query = `SELECT * FROM modulelist WHERE deviceId = '${req.query.deviceId}'`;
+
+    connection.query(query,(err, rows, fields) => {
+      if(!err) {
+        res.render('modifyModule', {'moduleInfo':rows.map(result => {
+                                                            return {
+                                                              deviceId: result.deviceId,
+                                                              deviceType: result.deviceType,
+                                                              location: result.location,
+                                                              rgstDt: moment(result.rgstDt).format('YYYY-MM-DD')
+                                                            }
+                                                          })
+                    });
+      } else {
+        res.send(err);
+      }
+    });
+  }
+});
+
+// 수정 ajax에 대한 응답
+router.get('/api/modify', function(req, res, next) {
+  const params = req.query;
+  const query = `UPDATE moduleList SET deviceType='${params.deviceType}', location='${params.location}'
+                 WHERE deviceId = '${params.deviceId}'`;
+
+  connection.query(query,(err, rows, fields) => {
+    if(!err) {
+      res.send("Success");
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+// 타입을 받아오는 ajax에 대한 응답
+router.get('/api/findType', function(req, res, next) {
+  const params = req.query;
+  const query = `SELECT deviceType FROM moduleList WHERE deviceId = '${params.deviceId}' `;
+
+  connection.query(query,(err, rows, fields) => {
+    if(!err) {
+      res.send(rows[0].deviceType);
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+module.exports = router;
